9564b511952dadafb2ff6a1b3e70f58133b73475
Anthony Nichols <anthony.nichols@bngholdingsinc.com>
2022-03-28 13:47:19 -0500

    init

---
+++ Benchmarks/PriorityQBench.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConnectBooster.Identity.Data.Utilities;
+
+public record UserEventLog(Guid Id, Guid UserId, int TypeId, string EventData, int RandI, DateTime CreatedDate);
+
+public sealed class UserEventLogger : IDisposable
+{
+    private readonly PriorityQueue<UserEventLog, int> _queueedEvents = new();
+    private readonly Task? _mainTask;
+    private Int64 _maxRandI;
+
+    private List<UserEventLog> GetUserEventLogsThreshold_H()
+    {
+        var lockTaken = false;
+        List<UserEventLog> events = new();
+        try
+        {
+            Monitor.TryEnter(_queueedEvents, ref lockTaken);
+            if (!lockTaken)
+            {
+                return events;
+            }
+
+            var startingCount = _queueedEvents.Count;
+            var array1 = new UserEventLog[startingCount + 1];
+
+            // Dequeue 4 (8 possible) at a time- should should maximize the op number per thread.
+            for (var i = 0; i <= startingCount; ++i)
+            {
+                array1[Convert.ToByte(_queueedEvents.TryDequeue(out var cEvent1, out _)) * i] = cEvent1!;
+                //if (_queueedEvents.TryDequeue(out var cEvent1, out _))
+                //{
+                //    events.Add(cEvent1);
+                //}
+            }
+
+            //// create four dynamic arrays
+            //var arraySize = startingCount / 4 + 1;
+            //var array1 = new UserEventLog[arraySize];
+            //var array2 = new UserEventLog[arraySize];
+            //var array3 = new UserEventLog[arraySize];
+            //var array4 = new UserEventLog[arraySize];
+
+            //// Dequeue 4 (8 possible) at a time- should should maximize the op number per thread.
+            //for (var i = 1; i <= arraySize + 1; ++i)
+            //{
+            //    array1[Convert.ToInt16(_queueedEvents.TryDequeue(out var cEvent1, out _)) * i] = cEvent1!;
+            //    array2[Convert.ToInt16(_queueedEvents.TryDequeue(out var cEvent2, out _)) * i] = cEvent2!;
+            //    array3[Convert.ToInt16(_queueedEvents.TryDequeue(out var cEvent3, out _)) * i] = cEvent3!;
+            //    array4[Convert.ToInt16(_queueedEvents.TryDequeue(out var cEvent4, out _)) * i] = cEvent4!;
+            //}
+
+            //events.AddRange(array1.Skip(1));
+            //events.AddRange(array2.Skip(1));
+            //events.AddRange(array3.Skip(1));
+            //events.AddRange(array4.Skip(1));
+        }
+        finally
+        {
+            if (lockTaken)
+            {
+                // ensure event logs do not get stuck- may loose logs if DQ fails
+                _queueedEvents.Clear();
+                Monitor.Exit(_queueedEvents);
+            }
+        }
+
+        return events;
+    }
+
+    private List<UserEventLog> GetUserEventLogsThreshold_L()
+    {
+        var lockTaken = false;
+        List<UserEventLog> events = new();
+        try
+        {
+            Monitor.TryEnter(_queueedEvents, ref lockTaken);
+            if (!lockTaken)
+            {
+                return events;
+            }
+
+            var startingCount = _queueedEvents.Count;
+            // Dequeue 4 (8 possible) at a time- should should maximize the op number per thread.
+            for (var i = 0; i <= startingCount; ++i)
+            {
+                if (_queueedEvents.TryDequeue(out var cEvent1, out _))
+                {
+                    events.Add(cEvent1);
+                }
+            }
+        }
+        finally
+        {
+            if (lockTaken)
+            {
+                // ensure event logs do not get stuck- may loose logs if DQ fails
+                _queueedEvents.Clear();
+                Monitor.Exit(_queueedEvents);
+            }
+        }
+
+        return events;
+    }
+
+    public void ProcessQueue_H()
+    {
+        try
+        {
+            foreach (var chunk in GetUserEventLogsThreshold_H().Chunk(1000))
+            {
+                // create intense math problem
+                _maxRandI = chunk.Aggregate(0L, (c, x) => c + x.RandI);
+
+                //// Create new _dbContext to avoid threading issues.
+                //using var dbContext = CreateDbContext();
+
+                //// disable default options that impact performance
+                //// and are not needed.
+                ////
+                //// note: if this is too slow (>2s per 1000 records)- we should
+                //// look into System.Data.SqlClient.SqlBulkCopy or
+                //// EFCore.BulkExtensions.
+                //dbContext.Database.AutoTransactionsEnabled = false;
+                //dbContext.Database.AutoSavepointsEnabled = false;
+
+                //dbContext.UserEventLogs.AddRange(chunk);
+                //var savedC = dbContext.SaveChanges();
+                //dbContext.Dispose();
+            }
+        }
+        finally
+        {
+            // process queue every minute - might need to expand.
+            // Thread.Sleep(60000);
+        }
+    }
+
+    public void ProcessQueue_L()
+    {
+        try
+        {
+            foreach (var chunk in GetUserEventLogsThreshold_L().Chunk(1000))
+            {
+                // create intense math problem
+                _maxRandI = chunk.Aggregate(0L, (c, x) =>  c + x.RandI);
+
+                //// Create new _dbContext to avoid threading issues.
+                //using var dbContext = CreateDbContext();
+
+                //// disable default options that impact performance
+                //// and are not needed.
+                ////
+                //// note: if this is too slow (>2s per 1000 records)- we should
+                //// look into System.Data.SqlClient.SqlBulkCopy or
+                //// EFCore.BulkExtensions.
+                //dbContext.Database.AutoTransactionsEnabled = false;
+                //dbContext.Database.AutoSavepointsEnabled = false;
+
+                //dbContext.UserEventLogs.AddRange(chunk);
+                //var savedC = dbContext.SaveChanges();
+                //dbContext.Dispose();
+            }
+        }
+        finally
+        {
+            // process queue every minute - might need to expand.
+            // Thread.Sleep(60000);
+        }
+    }
+
+    public void EnqueueEvent(UserEventLog eventLog)
+    {
+        var lockTaken = false;
+        try
+        {
+            Monitor.TryEnter(_queueedEvents, ref lockTaken);
+            if (!lockTaken)
+            {
+                return;
+            }
+            // Millisecond should be a large enough buffer to avoid duplicate priorities.
+            _queueedEvents.Enqueue(eventLog, ((int)eventLog.TypeId + 1) * eventLog.CreatedDate.Millisecond);
+        }
+        finally
+        {
+            if (lockTaken)
+            {
+                Monitor.Exit(_queueedEvents);
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _mainTask?.Dispose();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ConnectBooster.Identity.Data.Utilities;
+using slugger;
+
+//BenchmarkRunner.Run<Md5VsSha256>();
+
+//BenchmarkRunner.Run<NullCoalescingOverIf>();
+
+BenchmarkRunner.Run<PriorityQB>();
+
+namespace slugger
+{
+    [MemoryDiagnoser]
+    public class Md5VsSha256
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256()
+        {
+            return sha256.ComputeHash(data);
+        }
+
+        [Benchmark]
+        public byte[] Md5()
+        {
+            return md5.ComputeHash(data);
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class NullCoalescingOverIf
+    {
+        //|        Method |      Mean |     Error |    StdDev |
+        //|-------------- |----------:|----------:|----------:|
+        //| NullCoalescer | 0.4996 ns | 0.0079 ns | 0.0070 ns |
+        //|          Ifer | 0.2863 ns | 0.0213 ns | 0.0200 ns |
+
+        //private readonly IEnumerable<object>? list = Enumerable.Empty<object>();
+        private readonly IEnumerable<object>? list = null;
+
+        public NullCoalescingOverIf() { }
+
+        [Benchmark]
+        public void NullCoalescer()
+        {
+            if (!list?.Any() ?? true)
+            {
+                return;
+            }
+        }
+
+        // Winner
+        [Benchmark]
+        public void Ifer()
+        {
+            if (list == null || !list.Any())
+            {
+                return;
+            }
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class PriorityQB
+    {
+        private const int N = 1000000;
+        private readonly UserEventLogger listH = new();
+        private readonly UserEventLogger listL = new();
+
+        public PriorityQB()
+        {
+            // for N
+            for (var i = 0; i < N; ++i)
+            {
+                var type = new Random(42).Next(0, 10);
+                var random = new Random(42).Next();
+                listL.EnqueueEvent(new UserEventLog(Guid.NewGuid(), Guid.NewGuid(), type, "", random, DateTime.Now));
+                listH.EnqueueEvent(new UserEventLog(Guid.NewGuid(), Guid.NewGuid(), type, "", random, DateTime.Now));
+            }
+        }
+
+        [Benchmark]
+        public void PriorityL()
+        {
+            listL.ProcessQueue_L();
+        }
+
+        [Benchmark]
+        public void PriorityH()
+        {
+            listH.ProcessQueue_H();
+        }
+    }
+}
